ef3262ea1c645ef3b13178b000dc6295b5006b83
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2022-05-31 00:48:04 -0400

    final commit with working timers

--- Sailing-Timer/Sailing-Timer.ino
+++ Sailing-Timer/Sailing-Timer.ino
@@ -14,15 +14,47 @@ void loop() {
   if(digitalRead(oneMin)){
     Serial.println("ONE MIN IS PRESSED");
     attention();
+    delay(1500);
+    oneMinute();
   }
 
   if(digitalRead(twentySec)){
     Serial.println("TWENTY SEC IS PRESSED");
-    //attention();
+    attention();
+    delay(1500);
     twentySeconds();
   }
 
 }
+
+void oneMinute(){
+  // One Minute
+    tone(buzzers, 450); 
+    delay(1000);        
+    noTone(buzzers);
+
+    delay(29000);
+
+    tone(buzzers, 450); 
+    delay(300);        
+    noTone(buzzers);
+    delay(300);        
+    tone(buzzers, 450);
+    delay(300);       
+    noTone(buzzers);
+    delay(300);       
+    tone(buzzers, 450); 
+    delay(300);        
+    noTone(buzzers);
+
+    delay(8500);
+    
+  // Twenty Seconds and Beyond...
+    twentySeconds();
+}
+
+
+
 // Twenty Second Timer Function
 void twentySeconds(){
   // Twenty Seconds
@@ -42,6 +74,7 @@ void twentySeconds(){
     noTone(buzzers);
 
     delay(4700);
+    
   // Five - One
     for(int i = 1;i <=5; i++){
       tone(buzzers, 450); 
